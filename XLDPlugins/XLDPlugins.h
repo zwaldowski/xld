766fd5443f1b4c6e4869925f700fd921f1781bac
Zachary Waldowski <zwaldowski@gmail.com>
2013-10-28 10:24:17 -0400

    Add XLDPlugins framework. Improve framework dependency sharing.

---
+++ XLDPlugins/XLDPlugins.h
@@ -0,0 +1,13 @@
+//
+//  XLDPlugins.h
+//  XLDPlugins
+//
+//  Created by Zach Waldowski on 10/28/13.
+//  Copyright (c) 2013 Taihei Monma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLDPlugins : NSObject
+
+@end
---
+++ XLDPlugins/XLDPlugins.m
@@ -0,0 +1,13 @@
+//
+//  XLDPlugins.m
+//  XLDPlugins
+//
+//  Created by Zach Waldowski on 10/28/13.
+//  Copyright (c) 2013 Taihei Monma. All rights reserved.
+//
+
+#import "XLDPlugins.h"
+
+@implementation XLDPlugins
+
+@end
